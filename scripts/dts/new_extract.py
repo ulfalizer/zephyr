07e0125b56daddf7a2ba540f594a0e14613e428b
Ulf Magnusson <Ulf.Magnusson@nordicsemi.no>
2019-07-12 22:14:22 +0200

    Shorten comment that gets redundant with out_comment()

--- scripts/dts/new_extract.py
+++ scripts/dts/new_extract.py
@@ -56,13 +56,9 @@ def main():
 
             active_compats.update(dev.compats)
 
-    # Generate defines of the form
-    #
-    #   #define DT_COMPAT_<COMPAT> 1
-    #
-    # These are flags for which compats exist on (enabled) DT nodes.
     out_comment("Active compatibles (mentioned in DTS + binding found)")
     for compat in active_compats:
+        #define DT_COMPAT_<COMPAT> 1
         out("COMPAT_{}".format(str2ident(compat)), 1)
 
     # These are derived from /chosen
